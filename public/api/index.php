07b2fc130f2b65ad00b960b4a56fbeaca2715975
karek314 <karek314@protonmail.ch>
2018-01-16 16:56:54 +0100

    content type fix

--- public/api/index.php
+++ public/api/index.php
@@ -2,5 +2,6 @@
 error_reporting(error_reporting() & ~E_NOTICE);
 $m = new Memcached();
 $m->addServer('localhost', 11211);
+header('Content-Type: application/json');
 echo json_encode($m->get('pending_tx'),true);
 ?>
