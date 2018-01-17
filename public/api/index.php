f10a8ed5439ac4ef3040061a6f446546643404c3
karek314 <karek314@protonmail.ch>
2018-01-17 21:01:59 +0100

    origin wildcard

--- public/api/index.php
+++ public/api/index.php
@@ -3,5 +3,6 @@ error_reporting(error_reporting() & ~E_NOTICE);
 $m = new Memcached();
 $m->addServer('localhost', 11211);
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
 echo json_encode($m->get('pending_tx'),true);
 ?>
